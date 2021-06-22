fbd76e433f4bf75b9c2f895164cd28835f706e0b
VictorDauph <victor.dauph@gmail.com>
2021-06-22 16:02:35 +0200

    validation W3C

--- custom_code/scripts.js
+++ custom_code/scripts.js
@@ -15,4 +15,6 @@ let display = document.getElementById('displ');
     .then(data => console.log(data))
     .catch(error => console.log("error from frontend!!")) */
 
-    fetch("http://localhost:3000/api/teddies").then(res=>res.json()).then(data=> console.log(data) );
+    //fetch("http://localhost:3000/api/teddies").then(res=>res.json()).then(data=> console.log(data) );
+
+    console.log("coucou");
