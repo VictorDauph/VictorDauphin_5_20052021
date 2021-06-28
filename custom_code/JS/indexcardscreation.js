db26c60dd0ec06f7fd06d6136f96b8ad30c3dcff
VictorDauph <victor.dauph@gmail.com>
2021-06-28 11:53:36 +0200

    script affichage cartes index

--- custom_code/JS/indexcardscreation.js
+++ custom_code/JS/indexcardscreation.js
@@ -23,50 +23,76 @@ fetch('http://localhost:3000/api/teddies')
 
 
 
-// fonction de génération de cartes page principale
-function displayProduct(card) 
+// génération de cartes produits page principale
+//la classe productCards produit une carte produit
+
+class productCards
+{
+    constructor(cardType,product) //cardType fournit l'URL à aller chercher (ficher variables.js) et cardNumber correspond à l'index de l'objet à aller cherher.
     {
-        eraseProductsContainer()
-        fetch(card.url)
-        .then(res => res.json()) // on parse la réponse de la requête en json
-        //.then(data => console.log(data)) //on affiche le résultat du parsing (array Json) dans la console
-        .then(data => {
-                        //création des variables fetch
-                        const pictureCardUrl = data[0].imageUrl;
-                        const cardTitleContent = document.createTextNode(data[0].name);
-                        const cardDescriptionContent = document.createTextNode(data[0].description);
-                        
-                        
-                        //création du cadre de la carte
-                        const linkContainerCard = document.createElement("a");
-                        productsContainer.appendChild(linkContainerCard);
-                        linkContainerCard.classList.add("anim", "card", "col-md-3", "bg-light", "shadow", "text-decoration-none", "gx-0", "m-3");
-                        linkContainerCard.setAttribute("href","./custom_code/pages_HTML/product.html")
+        fetch(cardType.url)
+            .then(res => res.json()) // on parse la réponse de la requête en json
+            //.then(data => console.log(data)) //on affiche le résultat du parsing (array Json) dans la console
+            .then(data => 
+                        {
                             
-                        //création image de la carte
-                        const pictureCardTag = document.createElement("img");
-                        linkContainerCard.appendChild(pictureCardTag);
-                        pictureCardTag.classList.add("card-img-top", "border-bottom", "border-secondary");
-                        pictureCardTag.setAttribute("src", pictureCardUrl );
+                                //création des variables fetch
+                                const pictureCardUrl = product.imageUrl;
+                                const cardTitleContent = document.createTextNode(product.name);
+                                const cardDescriptionContent = document.createTextNode(product.description);
+                                
+                                
+                                //création du cadre de la carte
+                                const linkContainerCard = document.createElement("a");
+                                productsContainer.appendChild(linkContainerCard);
+                                linkContainerCard.classList.add("anim", "card", "col-md-3", "bg-light", "shadow", "text-decoration-none", "gx-0", "m-3");
+                                linkContainerCard.setAttribute("href","./custom_code/pages_HTML/product.html")
+                                    
+                                //création image de la carte
+                                const pictureCardTag = document.createElement("img");
+                                linkContainerCard.appendChild(pictureCardTag);
+                                pictureCardTag.classList.add("card-img-top", "border-bottom", "border-secondary");
+                                pictureCardTag.setAttribute("src", pictureCardUrl );
 
-                        //création card-body
-                        const cardBody = document.createElement("div");
-                        linkContainerCard.appendChild(cardBody);
-                        cardBody.classList.add("card-body");
-                        
-                        //création titre
-                        const cardTitleTag = document.createElement("h3");
-                        cardTitleTag.classList.add("card-title", "text-center");
-                        cardBody.appendChild(cardTitleTag);
-                        cardTitleTag.appendChild(cardTitleContent);
+                                //création card-body
+                                const cardBody = document.createElement("div");
+                                linkContainerCard.appendChild(cardBody);
+                                cardBody.classList.add("card-body");
+                                
+                                //création titre
+                                const cardTitleTag = document.createElement("h3");
+                                cardTitleTag.classList.add("card-title", "text-center");
+                                cardBody.appendChild(cardTitleTag);
+                                cardTitleTag.appendChild(cardTitleContent);
 
-                        //création description
-                        const cardDescriptionTag = document.createElement("p");
-                        cardDescriptionTag.classList.add("card-text");
-                        cardBody.appendChild(cardDescriptionTag);
-                        cardDescriptionTag.appendChild(cardDescriptionContent);
-                    
+                                //création description
+                                const cardDescriptionTag = document.createElement("p");
+                                cardDescriptionTag.classList.add("card-text");
+                                cardBody.appendChild(cardDescriptionTag);
+                                cardDescriptionTag.appendChild(cardDescriptionContent);
+                        })}}
+
+
+
+
+    function displayProduct(cardType) 
+        {
+            eraseProductsContainer()
+            fetch(cardType.url)
+            .then(res => res.json()) // on parse la réponse de la requête en json
+            .then(data => {
                 
-            }) 
-    }
+                data.forEach(item => {
+                    console.log(item._id)
+                    let card = new productCards(cardType,item)
+                })
+            
+                        
+                    
+        })} 
+    
+
+
+
+
 
