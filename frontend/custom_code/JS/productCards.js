7f34d1151608213e35f445096cc54a00d31b018e
VictorDauph <victor.dauph@gmail.com>
2021-06-30 17:26:05 +0200

    mise à jour cartes prduits et test d'affichage carte seule

--- frontend/custom_code/JS/productCards.js
+++ frontend/custom_code/JS/productCards.js
@@ -24,7 +24,7 @@ function eraseProductsContainer() //effacer le conteneur
 
 
 
-// les fonctions displays appellent la fonction d'affichage en passant des arguments différents. Ces arguments sont les noms des objets à afficher.
+// selectDisplay prend l'URL dans le localstrorage puis la fecth et appelle la fonction d'afficgahe
 
 function selectDisplay()
     {
@@ -32,41 +32,76 @@ function selectDisplay()
         console.log("LocalStorage récupéré: ", urlToFetch);
         displayProduct(urlToFetch);
     }
+
+// Pilotage de l'affichage des éléments de la page.
+function displayProduct(urlToFetch) 
+    {
+        console.log("display product lancé: ", urlToFetch);
+        eraseProductsContainer()
+        titleSelection(urlToFetch)
+        fetch(urlToFetch) 
+        .then(res => res.json()) // on parse la réponse de la requête en json
+        .then(data => {
+            
+            data.forEach(item => {
+                let card = new productCards(urlToFetch,item) //cette boucle construit des instances de la classe product Cards: les cartes produits.
+            })     
+    })} 
+
+//Selection du titre de la page
+function titleSelection(selection)
+{
+    
+    if (selection == "http://localhost:3000/api/teddies")
+        {   console.log("titre = teddies");
+            titreProduit.innerHTML = "Nos Ours en peluche!"; 
+        }
+    else if (selection == "http://localhost:3000/api/cameras")
+    {   console.log("titre = cameras");
+    titreProduit.innerHTML = "Nos caméras!";
+    }
+    else if (selection == "http://localhost:3000/api/furniture")
+    {   console.log("titre = furniture");
+    titreProduit.innerHTML = "Nos meubles en chêne!";  
+    }
+}
     
 //la classe productCards produit une carte produit
 
 class productCards
 {
-    constructor(cardType,product) //cardType fournit l'URL à aller chercher et prodcut correspond à l'index de l'objet à aller cherher.
+    constructor(cardType,product) //cardType fournit l'URL à aller chercher et product correspond à l'index de l'objet à aller cherher.
     {
         fetch(cardType)// remplacer Url par variable cardType
             .then(res => res.json()) // on parse la réponse de la requête en json
             //.then(data => console.log(data)) //on affiche le résultat du parsing (array Json) dans la console
             .then(data => 
                         {
-                            
                                 //création des variables fetch
                                 const pictureCardUrl = product.imageUrl;
                                 const cardTitleContent = document.createTextNode(product.name);
                                 const cardDescriptionContent = document.createTextNode(product.description);
                                 const optionName = Object.keys(product)[0];
                                 const optionChoices = Object.values(product)[0];
+                                const idCard = product._id;
+                                
 
                                 //création du cadre de la carte
-                                const linkContainerCard = document.createElement("div");
-                                productsContainer.appendChild(linkContainerCard);
-                                linkContainerCard.classList.add("anim", "card", "col-md-3", "bg-light", "shadow", "text-decoration-none", "gx-0", "m-3");
-                                
+                                const containerCard = document.createElement("a");
+                                productsContainer.appendChild(containerCard);
+                                containerCard.classList.add("anim", "card", "col-md-3", "bg-light", "shadow", "text-decoration-none", "gx-0", "m-3", "cursor-pointer");
+                                containerCard.id = idCard;
+                                containerCard.href = "#productsContainer";
                                     
                                 //création image de la carte
                                 const pictureCardTag = document.createElement("img");
-                                linkContainerCard.appendChild(pictureCardTag);
+                                containerCard.appendChild(pictureCardTag);
                                 pictureCardTag.classList.add("card-img-top", "border-bottom", "border-secondary");
                                 pictureCardTag.setAttribute("src", pictureCardUrl );
 
                                 //création card-body
                                 const cardBody = document.createElement("div");
-                                linkContainerCard.appendChild(cardBody);
+                                containerCard.appendChild(cardBody);
                                 cardBody.classList.add("card-body");
                                 
                                 //création titre
@@ -82,16 +117,20 @@ class productCards
                                 cardDescriptionTag.appendChild(cardDescriptionContent);
 
                                 //création choix de personnalisation
+                                const customOptionsContainer = document.createElement("div");
+                                cardBody.appendChild(customOptionsContainer);
+                                customOptionsContainer.classList.add("d-none");
+
                                 //label
                                 const cardOptionLabel = document.createElement("label");
-                                cardBody.appendChild(cardOptionLabel);
+                                customOptionsContainer.appendChild(cardOptionLabel);
                                 cardOptionLabel.classList.add("d-block", "mb-3");
                                 const cardOptionLabelContent = document.createTextNode(optionName + " options: ");
                                 cardOptionLabel.appendChild(cardOptionLabelContent);
 
                                 //tag liste d'options
                                 const cardOptionListTag = document.createElement("select");
-                                cardBody.appendChild(cardOptionListTag);
+                                customOptionsContainer.appendChild(cardOptionListTag);
 
                                 //création des listes de sélection d'options avec boucle forEach
                                 OptionListCreation(optionChoices)                              
@@ -109,55 +148,32 @@ class productCards
                             
                                 //création input nombre à ajouter au panier
                                 const numberInputLabel = document.createElement("label");
-                                linkContainerCard.appendChild(numberInputLabel);
+                                customOptionsContainer.appendChild(numberInputLabel);
                                 const numberInputLabelContent = document.createTextNode("Commander:");
                                 numberInputLabel.appendChild(numberInputLabelContent);
                                 const numberInputBasket = document.createElement("input");
-                                linkContainerCard.appendChild(numberInputBasket);
+                                customOptionsContainer.appendChild(numberInputBasket);
                                 numberInputLabel.classList.add("mx-3", "mb-3");
                                 numberInputBasket.setAttribute("type","number");
-                                numberInputBasket.classList.add("w-25", "mx-3","numberInput");
-
-                               
-                        })}}
+                                numberInputBasket.classList.add("w-25", "mx-3","numberInput"); 
 
 
+                                
+                                containerCard.addEventListener('click', selectId);
 
-// Pilotage de l'affichage des éléments de la page.
-    function displayProduct(urlToFetch) 
-        {
-            console.log("display product lancé: ", urlToFetch);
-            eraseProductsContainer()
-            titleSelection(urlToFetch)
-            fetch(urlToFetch) 
-            .then(res => res.json()) // on parse la réponse de la requête en json
-            .then(data => {
-                
-                data.forEach(item => {
-                    let card = new productCards(urlToFetch,item)
-                })
-            
-                        
-                    
-        })} 
+                                function selectId()
+                                {
+                                    displaySelectedProduct(idCard); // Créer une fonction qui efface le contenu du container et affiche une nouvelle carte
+                                }
 
-//Selection du titre de la page
-    function titleSelection(selection)
-        {
-            
-            if (selection == "http://localhost:3000/api/teddies")
-                {   console.log("titre = teddies");
-                    titreProduit.innerHTML = "Nos Ours en peluche!"; 
-                }
-            else if (selection == "http://localhost:3000/api/cameras")
-            {   console.log("titre = cameras");
-            titreProduit.innerHTML = "Nos caméras!";
-            }
-            else if (selection == "http://localhost:3000/api/furniture")
-            {   console.log("titre = furniture");
-            titreProduit.innerHTML = "Nos meubles en chêne!";  
-            }
-        }
+                                
+                                })}}
+
+
+
+
+
+
     
 selectDisplay() //lancer la selection et l'affichage au chargement de la page.
 
@@ -180,3 +196,13 @@ console.log ( testObjectIsBack.vegetable);
 // Comment l'utilisateur ajoute t-il un élément au panier? il entre un nombre dans une case, les cases mettent à jour automatiquement des variables... On utilise les ID pour relier un produit au nombre d'éléments dans le panier...
 //quand on valide le panier avec l'un des deux boutons les variables sont assignées à un objet, qui est transmis à la page panier avec localstorage.
 
+
+function displaySelectedProduct(idCard)
+{
+    eraseProductsContainer();
+    productsContainer.innerHTML = idCard;
+    fetch('http://localhost:3000/api/cameras')
+        .then( res => res.json())
+        .then( data => console.log(data))
+                                    
+}                           
